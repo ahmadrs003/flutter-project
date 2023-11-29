a625fca8b7142d93e31e74f1755f39664e980ce8
ahmadrs003 <152165379+ahmadrs003@users.noreply.github.com>
2023-11-29 18:29:00 +0300

    Update main.dart

--- edite_profile/main.dart
+++ edite_profile/main.dart
@@ -12,13 +12,23 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var _formKey = GlobalKey<FormState>();
-  TextEditingController myController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  final _passwordController = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          centerTitle: true,
+          title: Text("Edite Profile",
+          style: TextStyle(
+             color: Colors.black
+          ),),
+        ),
         body: Form(
           key: _formKey,
           child: Column(
@@ -29,9 +39,24 @@ class _MyAppState extends State<MyApp> {
               SizedBox(height: 30,),
 
       
-              Text("Profile Information"),
-                
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 40),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text("Profile Information"),
+                    SizedBox(width: 170,),
+                    Align(
+                      alignment: Alignment.bottomLeft,
+                      child: TextButton(
+                        onPressed: (){}, 
+                        child: Text("Save Information"),
+                      ),
+                    )
+                  ],
 
+                ),
+              ),
               SizedBox(height: 20,),
 
               Padding(
@@ -39,41 +64,74 @@ class _MyAppState extends State<MyApp> {
                 child: TextFormField(
                   decoration: InputDecoration(
                     labelText: "Name",
-                    hintText: " ",
+                    hintText: "Ahmad Mahmoud ",
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(15)
                     ),
                   ),
-                  validator: (value){
-                    if(value!.isEmpty){
-                    return "Can't be empty!.";
-                    }
-                    
-                  },
                 ),
               ),
-
               SizedBox(height: 10,),
 
               Padding(
                 padding: const EdgeInsets.only(top:10.0, left: 35.0, right: 35.0),
                 child: TextFormField(
                   decoration: InputDecoration(
-                    labelText: "Email",
-                    hintText: " ",
+                    label: Text("Email"),
+                    hintText: "ahmadmahmoud@gmail.com",
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(15)
                     ),
                   ),
                   validator: (value){
-                    if(value!.isEmpty){
-                    return "Can't be empty!.";
+                    if(value!.isEmpty || !value.contains("@")){
+                    return "Enter your email correctly.";
                     }
-                    
                   },
                 ),
               ),
 
+              Padding(
+                padding: EdgeInsets.only(top:20.0, left: 35.0, right: 35.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Password",
+                    hintText: "Password",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value!.length < 8) {
+                      return "Password length should be at least 8";
+                    }
+                    return null;
+                  },
+                  controller: _passwordController,
+                  obscureText: true,
+                ),
+              ),
+
+              Padding(
+                padding: EdgeInsets.only(top:20.0, left: 35.0, right: 35.0, bottom: 20.0 ),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Confirm Password",
+                    hintText: "Password",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value != _passwordController.text) {
+                      return "Passwords do not match";
+                    }
+                    return null;
+                  },
+                  controller: _confirmPasswordController,
+                  obscureText: true,
+                ),
+              ),
 
             ],
           ),
